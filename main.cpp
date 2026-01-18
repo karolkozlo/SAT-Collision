af9b85f324b627a09f016b6fae2f2e0c49c2c6ca
karol.kozlo <karol.kozlowski1999@gmail.com>
2026-01-18 17:26:02 +0100

    Draw first rectangle

--- main.cpp
+++ main.cpp
@@ -7,9 +7,24 @@ int main() {
     InitWindow(windowWidth, windowHeight, "SAT Collision");
     SetTargetFPS(60);
 
+    // Rect
+    int rectX = windowWidth / 4;
+    int rectY = windowHeight / 4;
+    int rectWidth = 60;
+    int rectHeight = 60;
+    int rectMoveSpeed = 8;
+    int rectYDirection = 1;
+    //Rect Collision
+    int lRectX{rectX};
+    int rRectX{rectX + rectWidth};
+    int uRectY{rectY};
+    int bRectY{rectY + rectHeight};
+
+
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(WHITE);
+        DrawRectangle(rectX, rectY, rectWidth, rectHeight, BLUE);
         EndDrawing();
     }
      CloseWindow();
