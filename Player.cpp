1b270776edc73767fe31dcf4a1631907e72e473e
karol.kozlo <karol.kozlowski1999@gmail.com>
2026-02-09 22:48:09 +0100

    Allow player to rotate

--- Player.cpp
+++ Player.cpp
@@ -1,6 +1,8 @@
 #include <Player.h>
 #include <raymath.h>
 
+const int ROTATION_SPEED = 50;
+
 Player::Player(Vector2 position, float width, float height, Color color, float rotation)
     : Entity(position, width, height, color, rotation) {
 
@@ -11,6 +13,12 @@ void Player::Tick(float deltaTime) {
     if (IsKeyDown(KEY_D)) velocity.x += 1.0;
     if (IsKeyDown(KEY_S)) velocity.y += 1.0;
     if (IsKeyDown(KEY_W)) velocity.y -= 1.0;
+    if (IsKeyDown(KEY_R)) {
+        rotation += ROTATION_SPEED * deltaTime;
+    }
+    if (IsKeyDown(KEY_F)) {
+        rotation -= ROTATION_SPEED * deltaTime;
+    }
 
     if (Vector2Length(velocity) != 0) {
         position = Vector2Add(position, Vector2Scale(Vector2Normalize(velocity), moveSpeed * deltaTime));
