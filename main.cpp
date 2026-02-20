2999f02d4ec1e97c5918e289e6732829654edc45
karol.kozlo <karol.kozlowski1999@gmail.com>
2026-02-20 22:19:27 +0100

    Change points for player and other entity

--- main.cpp
+++ main.cpp
@@ -10,17 +10,17 @@ int main() {
     SetTargetFPS(60);
 
     std::vector<Vector2> playerPoints = {
-        {0, 0},
-        {60, 0},
-        {60, 60},
-        {0, 60}
+        {-30, -30},
+        {30, -30},
+        {30, 30},
+        {-30, 30}
     };
     Player player{{windowWidth / 4, windowHeight / 4}, playerPoints, BLUE};
     std::vector<Vector2> otherPoints = {
         {0, 0},
         {60, 0},
-        {60, 60},
-        {0, 60}
+        {70, 60},
+        {-10, 60}
     };
     Entity otherThing{{(windowWidth / 50.f) + 400.f, (windowHeight / 4.f) + 100.f}, otherPoints, RED};
 
